245d245fa8ffd4520b32b76401a3c3bc7b34d604
Hyper_Contrast <183026749@qq.com>
2026-02-15 23:35:33 +0800

    :bug: config部分配置未使用

--- src/plugins/group_yashima/report/builder/manager.py
+++ src/plugins/group_yashima/report/builder/manager.py
@@ -1,15 +1,20 @@
 from .builder import ReportBuilder
 
+from src.plugins.group_yashima.config import get_config
+
 
 class ReportManager:
     def __init__(self):
         self.builder = ReportBuilder()
 
     def build_report_data(self):
-        self.builder.build_active_period_banner()
-        self.builder.build_active_user_count_banner()
-        self.builder.build_week_bar_plot()
-        self.builder.build_day_bar_plot()
+        if get_config().analyzer.big_banner:
+            self.builder.build_active_period_banner()
+            self.builder.build_active_user_count_banner()
+        if get_config().analyzer.week_bar_plot:
+            self.builder.build_week_bar_plot()
+        if get_config().analyzer.day_bar_plot:
+            self.builder.build_day_bar_plot()
 
     def get_report(self):
         return self.builder.get_report()
