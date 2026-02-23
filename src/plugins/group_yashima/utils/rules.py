4bcd978ca7c0398179ef8e8495ec98695a9f9b7f
Kontorasu <86454300+ConsetHP@users.noreply.github.com>
2026-02-23 23:31:40 +0800

    :bug: matcher的rule依賴注入 (#60)

--- src/plugins/group_yashima/utils/rules.py
+++ src/plugins/group_yashima/utils/rules.py
@@ -1,7 +1,7 @@
-from nonebot.adapters.onebot.v11 import GroupMessageEvent
+from nonebot_plugin_guild_patch import GuildMessageEvent
 
 from ..config import get_config
 
 
-async def guild_is_admin_user(event: GroupMessageEvent) -> bool:
+async def guild_is_admin_user(event: GuildMessageEvent) -> bool:
     return event.get_user_id() == str(get_config().general.bot_admin_tiny_id)
